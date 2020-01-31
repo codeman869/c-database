3d85c61bf6745fd88e0df9b047e9f726b1a95ec1
Cody Deckard <cjdeckard@gmail.com>
2020-01-31 21:03:03 +0000

    add testing

--- db.c
+++ db.c
@@ -11,8 +11,8 @@
 
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -28,7 +28,9 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_ID_NEGATIVE,
     PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR
 } PrepareResult;
 
@@ -76,6 +78,7 @@ ExecuteResult execute_select(Statement *, Table *);
 Table* new_table();
 void free_table(Table*);
 void print_row(Row*);
+PrepareResult prepare_insert(InputBuffer*, Statement*);
     
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
@@ -128,6 +131,12 @@ int main(int argc, char* argv[]) {
     switch(prepare_statement(input_buffer, &statement)) {
         case (PREPARE_SUCCESS):
             break;
+        case (PREPARE_STRING_TOO_LONG):
+            printf("String is too long.\n");
+            continue;
+        case (PREPARE_ID_NEGATIVE):
+            printf("ID must be positive.\n");
+            continue;
         case (PREPARE_SYNTAX_ERROR):
             printf("Syntax error. Could not parse statement.\n");
             continue;
@@ -161,15 +170,9 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement * statement) {
     if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",&(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email);
-        if(args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
+    
     if(strcmp(input_buffer->buffer, "select") == 0) {
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
@@ -255,4 +258,38 @@ void free_table(Table* table) {
 
 void print_row(Row* row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+PrepareResult prepare_insert(InputBuffer * input_buffer, Statement * statement ) {
+    statement->type = STATEMENT_INSERT;
+    
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+    
+    if(id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+    
+    int id = atoi(id_string);
+    
+    if(id < 0) {
+        return PREPARE_ID_NEGATIVE;
+    }
+    
+    if(strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    
+    if(strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+    
+    
+    return PREPARE_SUCCESS;
 }
