9414c3746bc7616a76fa0505808e7d4ab7287650
Cody Deckard <cjdeckard@gmail.com>
2020-02-03 16:31:26 +0000

    added Cursor abstraction

--- db.c
+++ db.c
@@ -63,6 +63,12 @@ typedef struct {
     Pager* pager;
 } Table;
 
+typedef struct {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table;
+} Cursor;
+
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row,username);
@@ -81,19 +87,22 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement * statement
 ExecuteResult execute_statement(Statement *, Table *);
 void serialize_row(Row*,void*);
 void deserialize_row(void*,Row*);
-void* row_slot(Table*,uint32_t);
+void* cursor_value(Cursor*);
 ExecuteResult execute_insert(Statement*, Table *);
 ExecuteResult execute_select(Statement *, Table *);
 Table* db_open(const char* filename);
-//void free_table(Table*);
 void print_row(Row*);
 PrepareResult prepare_insert(InputBuffer*, Statement*);
 Pager* pager_open(const char*);
 void* get_page(Pager*,uint32_t);
 void db_close(Table*);
 void pager_flush(Pager*, uint32_t, uint32_t);
-
+Cursor* table_start(Table*);
+void cursor_advance(Cursor*);
+Cursor* table_end(Table*);
+    
 InputBuffer* new_input_buffer() {
+    
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
@@ -209,19 +218,28 @@ ExecuteResult execute_insert(Statement* statement, Table * table) {
    }
    
    Row * row_to_insert = &(statement->row_to_insert);
-   serialize_row(row_to_insert, row_slot(table, table->num_rows));
+   Cursor* cursor = table_end(table);
+   serialize_row(row_to_insert, cursor_value(cursor));
    
    table->num_rows += 1;
+   
+   free(cursor);
+   
    return EXECUTE_SUCCESS;
    
 }
 
 ExecuteResult execute_select(Statement * statement, Table * table) {
+    Cursor* cursor = table_start(table);
     Row row;
-    for(uint32_t i = 0; i < table->num_rows; i++) {
-        deserialize_row(row_slot(table,i), &row);
+    
+    while(!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+    
+    free(cursor);
     
     return EXECUTE_SUCCESS;
 }
@@ -247,14 +265,16 @@ void deserialize_row(void* source, Row* destination) {
     
 }
 
-void* row_slot(Table* table, uint32_t row_num) {
-   uint32_t page_num = row_num / ROWS_PER_PAGE; 
+void* cursor_value(Cursor* cursor) {
+    
+    uint32_t row_num = cursor->row_num;
+    uint32_t page_num = row_num / ROWS_PER_PAGE; 
    
-   void* page = get_page(table->pager, page_num);
+    void* page = get_page(cursor->table->pager, page_num);
 
-   uint32_t row_offset = row_num % ROWS_PER_PAGE;
-   uint32_t byte_offset = row_offset * ROW_SIZE;
-   return page + byte_offset;
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
 }
 
 Table* db_open(const char* filename) {
@@ -443,4 +463,29 @@ void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
         exit(EXIT_FAILURE);
     }
     
+}
+
+Cursor* table_start(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+    
+    return cursor;
+}
+
+Cursor* table_end(Table* table) {
+    Cursor* cursor = malloc((sizeof(Cursor)));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+    
+    return cursor;
+}
+
+void cursor_advance(Cursor* cursor) {
+   cursor->row_num += 1; 
+   if(cursor->row_num >= cursor->table->num_rows) {
+       cursor->end_of_table = true;
+   }
 }
